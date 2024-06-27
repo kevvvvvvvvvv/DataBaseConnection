74dc6bb7d3500be4a653770a9bf68bc0ff7c9955
kevin <kevin@kevin>
2024-06-27 10:33:13 -0600

    Basa de datos

---
+++ src/testbasedataconnection/TestBaseDataConnection.java
@@ -0,0 +1,15 @@
+
+package testbasedataconnection;
+
+/* @author kevin
+ */
+public class TestBaseDataConnection {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
