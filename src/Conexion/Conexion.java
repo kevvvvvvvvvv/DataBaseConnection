de7803bca4ef394b815aa25ac9a71e5289c042bb
kevin <kevin@kevin>
2024-06-27 10:37:04 -0600

    Variable

---
+++ src/Conexion/Conexion.java
@@ -0,0 +1,14 @@
+
+package Conexion;
+
+import com.sun.jdi.connect.spi.Connection;
+import java.beans.Statement;
+
+/*@author kevin*/
+
+public class Conexion {
+    Connection conexion = null;
+    Statement comando = null;
+    ResultSet registro;
+    
+}
