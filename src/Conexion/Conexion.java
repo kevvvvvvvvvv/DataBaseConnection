526936587ad12e1ff0511fe89640e6f2a3dd190f
kevin <kevin@kevin>
2024-06-27 11:02:35 -0600

    Conexion terminada

--- src/Conexion/Conexion.java
+++ src/Conexion/Conexion.java
@@ -1,8 +1,10 @@
-
 package Conexion;
 
-import com.sun.jdi.connect.spi.Connection;
-import java.beans.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 /*@author kevin*/
 
@@ -11,4 +13,25 @@ public class Conexion {
     Statement comando = null;
     ResultSet registro;
     
+    public Connection JavaToMySQL() {
+        try {
+            // Instanciar librería del conector
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            
+            // Datos de la conexión
+            String servidor = "jdbc:mysql://localhost:3306/practica1";
+            String usuario = "root";
+            String password = "";
+            
+            // Establecer conexión
+            conexion = DriverManager.getConnection(servidor, usuario, password);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
+        return conexion;
+    }
 }
+
